4fe5f011c69ffca109f33e9362d0717390159c84
istamarahsan <istamarahsan@gmail.com>
2022-10-22 09:00:06 +0700

    Removed unused

--- script/game/EventDeck.gd
+++
@@ -1,4 +0,0 @@
-extends Resource
-class_name EventDeck
-
-export(Array, Array) var entries: Array
--- script/game/_EventDetails.gd
+++
@@ -1,9 +0,0 @@
-extends Resource
-class_name EventDetails
-
-export(String) var title: String
-export(String, MULTILINE) var description: String
-
-func _init(title = "", description = ""):
-	self.title = title
-	self.description = description
