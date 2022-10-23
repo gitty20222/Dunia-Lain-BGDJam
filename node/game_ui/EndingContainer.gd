d18092458dea037ab5fdf94326d21ef675a8b4ed
istamarahsan <istamarahsan@gmail.com>
2022-10-23 18:27:25 +0700

    ending data & text (pending images)

--- node/game_ui/EndingContainer.gd
+++ node/game_ui/EndingContainer.gd
@@ -2,6 +2,33 @@ extends MarginContainer
 
 const Enums = preload("res://script/Enums.gd")
 
+export(Texture) var pic_died: Texture
+export(Texture) var pic_depressed: Texture
+export(Texture) var pic_destitute: Texture
+export(Texture) var pic_healthy: Texture
+export(Texture) var pic_happy: Texture
+export(Texture) var pic_rich: Texture
+export(Texture) var pic_healthy_happy: Texture
+export(Texture) var pic_healthy_rich: Texture
+export(Texture) var pic_happy_rich: Texture
+export(Texture) var pic_healthy_happy_rich: Texture
+
+const title_died := "Meninggal"
+const title_depressed := "Depresi"
+const title_destitute := "Miskin"
+
+const title_healthy := "Bugar"
+const title_happy := "Ceria"
+const title_rich := "Kaya Raya"
+
+const desc_died := "Kesadaranku semakin memudar.."
+const desc_depressed := "Hidup adalah penderitaan"
+const desc_destitute := "Uang..aku butuh uang.."
+
+const desc_healthy := "Badanku terasa sangat segar dan bertenaga"
+const desc_happy := "Hidup terasa sangat menyenangkan seperti seharusnya"
+const desc_rich := "Uang adalah segalannya"
+
 class Ending:
 	var texture: Texture
 	var title: String
@@ -12,25 +39,37 @@ class Ending:
 		self.title = title
 		self.description = description
 
-var died := Ending.new()
+var died := Ending.new(pic_died, title_died, desc_died)
 
-var depressed := Ending.new()
+var depressed := Ending.new(pic_depressed, title_depressed, desc_depressed)
 
-var destitute := Ending.new()
+var destitute := Ending.new(pic_destitute, title_destitute, desc_destitute)
 
-var healthy := Ending.new()
+var healthy := Ending.new(pic_healthy, title_healthy, desc_healthy)
 
-var happy := Ending.new()
+var happy := Ending.new(pic_happy, title_happy, desc_happy)
 
-var rich := Ending.new()
+var rich := Ending.new(pic_rich, title_rich, desc_rich)
 
-var healthy_happy := Ending.new()
+var healthy_happy := Ending.new(
+	pic_healthy_happy, 
+	title_healthy + ", " + title_happy, 
+	desc_happy + "\n" + desc_healthy)
 
-var healthy_rich := Ending.new()
+var healthy_rich := Ending.new(
+	pic_healthy_rich,
+	title_healthy + ", " + title_rich,
+	desc_healthy + "\n" + desc_rich)
 
-var happy_rich := Ending.new()
+var happy_rich := Ending.new(
+	pic_happy_rich,
+	title_happy + ", " + title_rich,
+	desc_happy + "\n" + desc_rich)
 
-var healthy_happy_rich := Ending.new()
+var healthy_happy_rich := Ending.new(
+	pic_healthy_happy_rich,
+	title_healthy + ", " + title_happy + ", " + title_rich,
+	desc_healthy + "\n" + desc_happy + "\n" + desc_rich)
 
 func set_ending(ending):
 	var ending_data: Ending
@@ -55,7 +94,7 @@ func set_ending(ending):
 			ending_data = happy_rich
 		Enums.Ending.HealthyHappyRich:
 			ending_data = healthy_happy_rich
-			
+	
 	get_node("%TextureRect").texture = ending_data.texture
 	get_node("%EndingTitle").text = ending_data.title
 	get_node("%EndingText").text = ending_data.description
