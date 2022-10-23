eab98dfb841e37cfa2eed53297e85fb9631e79ae
istamarahsan <istamarahsan@gmail.com>
2022-10-23 11:01:24 +0700

    main scene skeleton

---
+++ node/Main.gd
@@ -0,0 +1,36 @@
+extends Node
+
+const SAVE_PATH = "user://save"
+
+func _ready():
+	pass
+
+func _on_Simulation_health_updated(old, new):
+	pass # Replace with function body.
+
+func _on_Simulation_happiness_updated(old, new):
+	pass # Replace with function body.
+
+func _on_Simulation_money_updated(old, new):
+	pass # Replace with function body.
+
+func _on_Simulation_fitness_value_updated(old, new):
+	pass # Replace with function body.
+
+func _on_Simulation_work_value_updated(old, new):
+	pass # Replace with function body.
+
+func _on_Simulation_social_value_updated(old, new):
+	pass # Replace with function body.
+
+func _on_Simulation_sleep_value_updated(old, new):
+	pass # Replace with function body.
+
+func _on_Simulation_status_added(status_id):
+	pass # Replace with function body.
+
+func _on_Simulation_status_removed(status_id):
+	pass # Replace with function body.
+
+func _on_Simulation_turn_resolved(game_state):
+	pass # Replace with function body.
