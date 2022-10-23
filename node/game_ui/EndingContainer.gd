fd5515d3180de19cdde4cd60daee62506625248b
christianseiji.cs@gmail.com <Ch28871c33@#>
2022-10-23 21:48:29 +0700

    ending done

--- node/game_ui/EndingContainer.gd
+++ node/game_ui/EndingContainer.gd
@@ -30,44 +30,57 @@ const desc_happy := "Hidup terasa sangat menyenangkan seperti seharusnya"
 const desc_rich := "Uang adalah segalannya"
 
 class Ending:
-	var texture: Texture
+	var texture: Array
 	var title: String
 	var description: String
 	
-	func _init(texture: Texture, title: String, description: String):
+	func _init(texture: Array, title: String, description: String):
 		self.texture = texture
 		self.title = title
 		self.description = description
 
-var died := Ending.new(pic_died, title_died, desc_died)
+var died := Ending.new([pic_died], title_died, desc_died)
 
-var depressed := Ending.new(pic_depressed, title_depressed, desc_depressed)
+var depressed := Ending.new([pic_depressed], title_depressed, desc_depressed)
 
-var destitute := Ending.new(pic_destitute, title_destitute, desc_destitute)
+var destitute := Ending.new([pic_destitute], title_destitute, desc_destitute)
 
-var healthy := Ending.new(pic_healthy, title_healthy, desc_healthy)
+var healthy := Ending.new([pic_healthy], title_healthy, desc_healthy)
 
-var happy := Ending.new(pic_happy, title_happy, desc_happy)
+var happy := Ending.new([pic_happy], title_happy, desc_happy)
 
-var rich := Ending.new(pic_rich, title_rich, desc_rich)
+var rich := Ending.new([pic_rich], title_rich, desc_rich)
 
 var healthy_happy := Ending.new(
-	pic_healthy_happy, 
+	[
+		pic_healthy,
+		pic_happy
+	], 
 	title_healthy + ", " + title_happy, 
 	desc_happy + "\n" + desc_healthy)
 
 var healthy_rich := Ending.new(
-	pic_healthy_rich,
+	[
+		pic_healthy,
+		pic_rich
+	],
 	title_healthy + ", " + title_rich,
 	desc_healthy + "\n" + desc_rich)
 
 var happy_rich := Ending.new(
-	pic_happy_rich,
+	[
+		pic_happy,
+		pic_rich
+	],
 	title_happy + ", " + title_rich,
 	desc_happy + "\n" + desc_rich)
 
 var healthy_happy_rich := Ending.new(
-	pic_healthy_happy_rich,
+	[
+		pic_healthy,
+		pic_happy,
+		pic_rich
+	],
 	title_healthy + ", " + title_happy + ", " + title_rich,
 	desc_healthy + "\n" + desc_happy + "\n" + desc_rich)
 
@@ -95,6 +108,26 @@ func set_ending(ending):
 		Enums.Ending.HealthyHappyRich:
 			ending_data = healthy_happy_rich
 	
-	get_node("%TextureRect").texture = ending_data.texture
+#	get_node("%TextureRect").texture = ending_data.texture
+	if ending_data.texture.size() == 1:
+		get_node("%TextureRect").texture = ending_data.texture[0]
+	
+	if ending_data.texture.size() == 2:
+		get_node("%TextureRect").texture = ending_data.texture[0]
+		get_node("%TextureRect3").texture = ending_data.texture[1]
+		
+	if ending_data.texture.size() == 2:
+		get_node("%TextureRect").texture = ending_data.texture[0]
+		get_node("%TextureRect3").texture = ending_data.texture[1]
+		get_node("%TextureRect4").texture = ending_data.texture[2]
+		get_node("%TextureRect2").texture = ending_data.texture[3]
+	
 	get_node("%EndingTitle").text = ending_data.title
 	get_node("%EndingText").text = ending_data.description
+
+
+
+func _on_MarginContainer_gui_input(event):
+	if event is InputEventMouseButton and event.pressed and event.button_index == BUTTON_LEFT:
+		get_tree().reload_current_scene()
+	pass # Replace with function body.
